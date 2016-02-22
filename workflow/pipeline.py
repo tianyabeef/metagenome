cc241deebff4833a3b269b4fddf3f7c2bdc7c077
huangyun <410378266@qq.com>
2016-02-22 16:02:48 +0800

    test

--- workflow/pipeline.py
+++ workflow/pipeline.py
@@ -4,6 +4,21 @@ __author__ = "huangy"
 __copyright__ = "Copyright 2016, The metagenome Project"
 __version__ = "1.0.0-dev"
 
-class Pipeline():
-    def __init__(self,name):
-        self.name = name
+from ConfigParser import ConfigParser
+import sys
+import node
+import argparse
+
+def read_params(args):
+    parsers = argparse.ArgumentParser(description='''The initial run script of metagene ''')
+    parsers.add_argument('--config', dest='config_path', metavar='FILE', type=str, required=True,
+                        help="config file for metagenome pipeline")
+    args = parsers.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    params = read_params(args)
+    config = params.config
+    print config
