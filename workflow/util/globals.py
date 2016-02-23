29fbf0961faa0ce2df6556c9cfe2e5bcb8e2cd1f
huangyun <410378266@qq.com>
2016-02-23 13:30:16 +0800

    globals

--- workflow/util/globals.py
+++ workflow/util/globals.py
@@ -4,6 +4,6 @@ __author__ = "huangy"
 __copyright__ = "Copyright 2016, The metagenome Project"
 __version__ = "1.0.0-dev"
 
-import const
+from workflow.util import const
 const.script_dir = "/data_center_01/pipeline/huangy/metagenome"
 const.config_default_dir = "%s/config/" % const.script_dir
