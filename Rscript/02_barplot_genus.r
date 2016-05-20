7b2c70d12506f14c01d34d258f926b2f1707cfa1
huangyun <410378266@qq.com>
2016-05-20 13:11:42 +0800

    modify config file

--- Rscript/02_barplot_genus.r
+++ Rscript/02_barplot_genus.r
@@ -1,6 +1,6 @@
-tax_level="species"
-profile <- read.table("../profile/species.profile", header = T, check.names = F,quote="",sep="\t")
-number  <- 15
+tax_level="genus"
+profile <- read.table("../profile/genus.profile", header = T, check.names = F,quote="",sep="\t"，row.names=1);
+number  <- 15;
 ## reorder
 #neworder = as.character(unlist(read.table("species/top20/20150717/bar/sample.order.list")))
 #profile = profile[, neworder]
@@ -17,6 +17,7 @@ for (i in colnames(profile)){
 Others  <- 1 - apply(profile, 2, sum);
 profile <- rbind(profile, Others);
 profile <- profile[which(apply(profile, 1, sum) > 0), ];
+profile <- as.matrix(profile);
 ## picture parameters
 palette <- c("red",        "gray",          "cornflowerblue", "chartreuse3",
 	     "yellow",     "honeydew4",     "indianred4",     "khaki",
@@ -28,20 +29,20 @@ width <- 2;
 ## calculate size
 
 ## draw picture
-pdf("species.barplot.pdf", height = 15, width = 15);
+pdf("genus.barplot.pdf", height = 15, width = 15);
 layout(matrix(c(1, 2), nrow = 2));
 par(oma   = c(2, 2, 2, 2),
     mar   = c(5, 5, 5, 5));
 #barplot(table, col = colorvector(colornumber), xaxt = "n", space = spa, width = width)
-if (ncol(profile) > 15){
+if (ncol(profile) > 15) {
   barplot(profile, col = color(nrow(profile)), space = space, width = width, las = 2)
 }else {
   barplot(profile, col = color(nrow(profile)), space = space, width = width)
 }
 #text(seq(from = width - 0.3,length = ncol(profile), by=2 * space + width),par("usr")[3] - 0.15,srt=90,adj=0.5,labels=gsub("^Q", "", colnames(table)),xpd=T,font=1,cex=2, pos = 1)
-mtext("15 Main Species in Each Sample", side = 3, line = 1, cex = 2)
+mtext("15 Main Genus in Each Sample", side = 3, line = 1, cex = 2)
 ## legend
-par(mar   = c(5, 5, 2, 2));
+par(mar   = c(5, 5, 2, 5));
 plot(0, type = "n", xaxt = "n", yaxt = "n", bty ="n", xlab = "", ylab = "");
 legend("top", pch = 15, col = rev(color(nrow(profile))), legend = rev(rownames(profile)), bty = "n", pt.cex = 2, ncol = 3);
 #text(seq(from = spa+0.5*width - 0.1,length = ncol(table), by=spa + width),par("usr")[3] - 0.06,srt=90,adj=0.5,labels=new_order,xpd=T,font=1,cex=0.9, pos = 1)
