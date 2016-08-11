9958682e4a67f5ee7f4e63eade540fb2a1e1e5ac
huangyun <410378266@qq.com>
2016-08-11 11:29:59 +0800

    02.taxon 提交

--- workflow/util/useful.py
+++ workflow/util/useful.py
@@ -8,3 +8,24 @@ import os
 def mkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
+def parse_group(group_file):
+    sample_set = set()
+    group_set = {}
+    with open(group_file) as group:
+        for line in group:
+            sample_name, group_name = line.strip().split('\t')
+            if group_name not in group_set:
+                group_set[group_name] = set()
+            group_set[group_name].add(sample_name)
+            sample_set.add(sample_name)
+    sample_num_in_groups = map(lambda s: len(s), group_set.itervalues())
+    min_sample_num_in_groups = min(sample_num_in_groups)
+    sample_num_total = len(sample_set)
+    group_num = len(group_set)
+    return sample_num_in_groups, min_sample_num_in_groups, sample_num_total, group_num
+def get_name(path):
+    basename = os.path.basename(path)
+    dirname = os.path.split(path)
+    filename = os.path.splitext(basename)[0]
+    suffix = os.path.splitext(basename)[1]
+    return dirname,filename,suffix
